55c74f66bf7f2ebd28e1a3f8fd2cf92292fcfa5e
Ruslan Gusseinov <r.gusseinov@mail.ru>
2025-03-22 23:30:53 +0500

    fix load balancer security group issue

--- main.tf
+++ main.tf
@@ -22,20 +22,6 @@ module "vpc" {
   enable_ipv6 = false
 }
 
-/* module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.7.1"
-
-  for_each = toset(["dev", "staging"])
-
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id = module.vpc.public_subnets[each.key == "dev" ? 0 : 1]
-
-  tags = {
-    Name = each.value
-  }
-} */
-
 resource "aws_security_group" "lb_public_access" {
   name   = "lb-public-access"
   vpc_id = module.vpc.vpc_id
@@ -60,32 +46,50 @@ resource "aws_security_group" "lb_public_access" {
 resource "aws_security_group" "ec2_lb_access" {
   name   = "ec2-lb-access"
   vpc_id = module.vpc.vpc_id
-}
 
-
-resource "aws_vpc_security_group_ingress_rule" "ec2_lb_access" {
-  security_group_id = aws_security_group.ec2_lb_access.id
-
-  from_port   = 80
-  to_port     = 80
-  ip_protocol = "tcp"
-
-  referenced_security_group_id = aws_security_group.lb_public_access.id
-}
-
-
-resource "aws_vpc_security_group_egress_rule" "ec2_internet_access" {
-  for_each          = toset(["80", "443"])
-  security_group_id = aws_security_group.ec2_lb_access.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = each.value
-  ip_protocol = "tcp"
-  to_port     = each.value
-
-  tags = {
-    Name = "internet access port ${each.value}"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.lb_public_access.id]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create classic load balancer / Elastic Load Balancer
+resource "aws_elb" "app" {
+  name               = "app"
+  # availability_zones = ["us-east-1a", "us-east-1b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port          = 80
+    lb_protocol      = "HTTP"
+  }
+
+  security_groups = [aws_security_group.lb_public_access.id]
+  subnets         = module.vpc.public_subnets
+
+  health_check {
+    target              = "HTTP:80/"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+  }
+}
+
+resource "aws_elb_attachment" "app" {
+  count         = length(aws_instance.app)
+  elb          = aws_elb.app.id
+  instance     = aws_instance.app[count.index].id
 }
 
 resource "aws_instance" "app" {
@@ -93,11 +97,9 @@ resource "aws_instance" "app" {
   ami                  = var.ami_id
   instance_type        = var.instance_type
   subnet_id            = module.vpc.private_subnets[count.index % length(module.vpc.private_subnets)]
-  key_name = var.key_name
+  key_name             = var.key_name
 
-  vpc_security_group_ids = [
-    aws_security_group.ec2_lb_access.id
-  ]
+  vpc_security_group_ids = [aws_security_group.ec2_lb_access.id]
 
   associate_public_ip_address = true
   user_data_replace_on_change = true
@@ -115,53 +117,5 @@ resource "aws_instance" "app" {
   tags = {
     Name = "app - ${count.index}"
     role = "app"
-  }
-}
-
-resource "aws_lb" "app" {
-  name               = "app"
-  internal           = false
-  load_balancer_type = "application"
-  enable_http2       = true
-  ip_address_type    = "ipv4"
-  security_groups = [
-    aws_security_group.lb_public_access.id
-  ]
-  subnets = module.vpc.public_subnets
-}
-
-
-resource "aws_lb_target_group" "app" {
-  name     = "app"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
-
-  health_check {
-    healthy_threshold   = 3
-    unhealthy_threshold = 10
-    timeout             = 5
-    interval            = 10
-    path                = "/"
-    port                = "80"
-  }
-}
-
-
-resource "aws_lb_target_group_attachment" "app" {
-  count            = length(aws_instance.app)
-  target_group_arn = aws_lb_target_group.app.arn
-  target_id        = aws_instance.app[count.index].id
-}
-
-
-resource "aws_lb_listener" "app-http" {
-  load_balancer_arn = aws_lb.app.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
   }
 }
